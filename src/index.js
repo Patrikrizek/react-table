7fb2fafa998eb4f84d404d3158d8dd66fd31069d
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-11-22 17:04:57 +0000

    Pulling in API Data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import Api from './Api'
 import './index.css'
 
 const container = document.getElementById('root');
 const root = createRoot(container)
 root.render(
     <React.StrictMode>
+        <h1>Table</h1>
         <App />
+        <h1>Pulling in API Data</h1>
+        <Api />
     </React.StrictMode>
 );
