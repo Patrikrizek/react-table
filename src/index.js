5f67021b37b62484ee5bf306f90da7a3b780ef1a
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2022-11-22 14:02:23 +0000

    App component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
+import App from './App'
 import './index.css'
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React!</h1>
-      </div>
-    )
-  }
-}
 
 const container = document.getElementById('root');
 const root = createRoot(container)
